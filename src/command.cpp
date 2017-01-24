67688649d4b444816043112d4d9644d62e833794
Luan Doan <luandoan263@gmail.com>
2017-01-24 07:55:52 -0500

    Initial commit streaming node and lower control level added

---
+++ src/command.cpp
@@ -0,0 +1,6 @@
+command=ptz_req&req="+req+"&param="+param+"&channel="+g_viewStatu[g_clicked].nchannel+"&stream="+g_channelStatu[g_viewStatu[g_clicked].nchannel
+
+
+# Control commands (reconfig for up):
+# Move right:
+
